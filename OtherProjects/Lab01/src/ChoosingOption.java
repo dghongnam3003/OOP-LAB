93ce55a9ee426d44be13d2b79131cabdefb1a4bb
dghongnam3003 <63090159+dghongnam3003@users.noreply.github.com>
2023-05-27 11:00:39 +0700

    Aims

---
+++ OtherProjects/Lab01/src/ChoosingOption.java
@@ -0,0 +1,20 @@
+import javax.swing.JOptionPane;
+
+public class ChoosingOption {
+    public static void main(String[] args) {
+        int option = JOptionPane.showConfirmDialog(null, "Do you want to change the first class ticket?");
+        JOptionPane.showMessageDialog(null, "You have chosen: " + (option == JOptionPane.YES_OPTION? "Co":"Khong"));
+        System.exit(0);
+    }
+}
+
+
+/* Question:
+	
+-	What happens if users choose “Cancel”?
+Answer: If users choose "Cancel", It will print "You've chosen: No"
+
+-	How to customize the options to users, e.g. only two options: “Yes” and “No”, OR “I do” and “I don’t” (Suggestion: Use Javadocs or using Eclipse/Netbean IDE help). 
+Answer: Change the option variable:
+          int option = JOptionPane.showConfirmDialog(null, "Do you want to change to the first class ticket?", null, JOptionPane.YES_OPTION);
+ */

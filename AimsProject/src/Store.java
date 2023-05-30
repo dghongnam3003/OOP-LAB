16943ab2b24eade30e9d6b3086740a3a8f381d6e
dghongnam3003 <63090159+dghongnam3003@users.noreply.github.com>
2023-05-30 11:09:02 +0700

    store

---
+++ AimsProject/src/Store.java
@@ -0,0 +1,33 @@
+
+public class Store {
+	private DigitalVideoDisc [] itemsInStore = new DigitalVideoDisc[0];
+	
+	public void addDVD(DigitalVideoDisc dvd) {
+		DigitalVideoDisc [] newItemsInStore = new DigitalVideoDisc[itemsInStore.length + 1];
+		for (int i = 0; i < itemsInStore.length; i++) {
+			newItemsInStore[i] = itemsInStore[i];
+		}
+		newItemsInStore[itemsInStore.length] = dvd;
+		itemsInStore = newItemsInStore;
+	}
+	
+	public void removeDVD(DigitalVideoDisc dvd) {
+		for (int i = 0; i < itemsInStore.length; i++) {
+			if (dvd.getId() == itemsInStore[i].getId()) {
+				DigitalVideoDisc [] newItemsInStore = new DigitalVideoDisc[itemsInStore.length -1];
+				for (int j = i+1; j < itemsInStore.length; j++) {
+					itemsInStore[j-1] = itemsInStore[j];
+				}
+				itemsInStore[itemsInStore.length-1] = null;
+				for (int j = 0; j < itemsInStore.length - 1; j++) {
+					newItemsInStore[j] = itemsInStore[j];
+				}
+				itemsInStore = newItemsInStore;
+			}
+		}
+	}
+	
+	public DigitalVideoDisc[] getItemsInStore() {
+	    return itemsInStore;
+	}
+}
---
+++ AimsProject/src/StoreTest.java
@@ -0,0 +1,29 @@
+
+public class StoreTest {
+	public static void main(String[] args) {
+		Store store = new Store();
+		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Godfather", "Crime", "Francis Ford Coppola", 10.99f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("The Dark Knight", "Action", "Christopher Nolan", 9.99f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Pulp Fiction", "Crime", "Quentin Tarantino", 8.99f);
+        store.addDVD(dvd1);
+        store.addDVD(dvd2);
+        store.addDVD(dvd3);
+        
+        // Print the items in the store
+        DigitalVideoDisc[] itemsInStore = store.getItemsInStore();
+        System.out.println("Items in store:");
+        for (int i = 0; i < itemsInStore.length; i++) {
+            System.out.println(itemsInStore[i].toString());
+        }
+        
+        // Remove a DVD from the store
+        store.removeDVD(dvd2);
+        
+        // Print the items in the store again
+        itemsInStore = store.getItemsInStore();
+        System.out.println("Items in store after removing " + dvd2.getTitle() + ":");
+        for (int i = 0; i < itemsInStore.length; i++) {
+            System.out.println(itemsInStore[i].toString());
+        }
+	}
+}
